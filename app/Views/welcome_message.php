08b7e0e78b364a818c8d5e2d9d2809188921ca1b
Your Name <you@example.com>
2023-03-17 02:00:02 +0700

    portalberita

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,35 @@
+<?= $this->extend('layout/page_layout') ?>
+
+<?= $this->section('content') ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 my-2 card">
+				<div class="card-body">
+					<h5 class="h5">Semarang</h5>
+					<p>Semarang adalah ibu kota Provinsi Jawa Tengah, Indonesia yang sekaligus menjadi pusat pemerintahan dan perekonomian dari Provinsi Jawa Tengah, Indonesia. Kota ini adalah kota metropolitan terbesar keenam di Indonesia setelah Jakarta, Surabaya, Bandung, Medan, dan Palembang. Sebagai salah satu kota yang berkembang di Pulau Jawa, Kota Semarang mempunyai jumlah penduduk sekitar 1,6 juta jiwa.[3] Kawasan mega-urban Semarang yang tergabung dalam wilayah metropolitan Kedungsepur (Kabupaten Kendal, Kabupaten Demak, Ungaran Kabupaten Semarang, Kota Salatiga, Kota Semarang, dan Purwodadi Kabupaten Grobogan) berpenduduk mencapai 7,3 juta jiwa, sekaligus sebagai wilayah metropolitan berpenduduk terbanyak keempat di Indonesia, setelah Jabodetabek (Jakarta), Gerbangkertosusilo (Surabaya), dan Bandung Raya. Dalam beberapa tahun terakhir, perkembangan Semarang yang signifikan ditandai pula dengan munculnya beberapa gedung pencakar langit yang tersebar di penjuru kota dan penataan kota dengan dibangunnya tempat-tempat ramah pejalan kaki. Perkembangan regional ini menunjukan peran strategis Kota Semarang terhadap roda perekonomian nasional.</p>
+				</div>
+			</div>
+			<div class="col-md-12 my-2 card">
+				<div class="card-body">
+					<h5 class="h5">Kepemimpinan</h5>
+					<p>Kota Semarang dipimpin oleh wali kota Dr.Hendrar Prihadi, S.E, M.M dan wakil wali kota Ir. Hj. Hevearita Gunaryanti Rahayu M.Sos. Kota ini terletak sekitar 498 km sebelah timur Jakarta, atau 312 km sebelah barat Surabaya, atau 621 km sebalah barat daya Banjarmasin (via udara). Semarang berbatasan dengan Laut Jawa di sebelah utara, Kabupaten Demak di sebelah timur, Kabupaten Semarang di sebelah selatan, dan Kabupaten Kendal disebelah barat. Kota Semarang memiliki luas wilayah administratif sebesar 373,70 km persegi, sekaligus merupakan administrasi kotamadya terluas di Pulau Jawa.</p>
+				</div>
+			</div>
+			<div class="col-md-12 my-2 card">
+				<div class="card-body">
+					<h5 class="h5">Asal-Usul</h5>
+					<p>Secara etimologis, nama "Semarang" berasal dari kata "sem", yang berarti "asam/pohon asam", dan kata "arang", yang berarti "jarang", yang digabungkan menjadi "asam yang jarang-jarang". Penamaan "Semarang" ini bermula ketika Ki Ageng Pandanaran I datang ke sebuah pulau bernama Pulau Tirang (dekat pelabuhan Bergota) dan melihat pohon asam yang jarang-jarang tumbuh berdekatan. Penamaan Kota Semarang ini sempat berubah saat zaman kolonialisme Hindia Belanda menjadi "Samarang". Kota Semarang merupakan satu dari tiga pusat pelabuhan (Jakarta dan Surabaya) penting bagi Hindia Belanda sebagai pemasok hasil bumi dari wilayah pedalaman Jawa.</p>
+				</div>
+			</div>
+			<div class="col-md-12 my-2 card">
+				<div class="card-body">
+					<h5 class="h5">Pembagian Wilayah</h5>
+					<p>Seperti kota besar lainya, Kota Semarang mengenal sistem pembagian wilayah kota yang terdiri atas: Semarang Tengah atau Semarang Pusat, Semarang Timur, Semarang Selatan, Semarang Barat, dan Semarang Utara. Pembagian wilayah kota ini bermula dari pembagian wilayah sub-residen oleh Pemerintah Hindia Belanda yang setingkat dengan kecamatan. Namun saat ini, pembagian wilayah kota ini berbeda dengan pembagian administratif wilayah kecamatan. Meskipun pembagian kota ini jarang dipergunakan dalam lingkungan Pemerintahan Kota Semarang. Namun pembagian kota ini digunakan untuk mempermudah dalam menerangkan suatu lokasi menurut letaknya terhadap pusat kota Semarang. Pembagian kota ini juga digunakan oleh beberapa instansi di lingkungan Kota Semarang untuk mempermudah jangkauan pelayanan, seperti PLN dan PDAM.
+</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?= $this->endSection() ?>
